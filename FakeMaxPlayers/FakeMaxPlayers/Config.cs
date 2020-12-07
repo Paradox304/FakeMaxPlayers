b8218d0af36de4d0ee22432403278d82a2516344
maazkashif492@outlook.com <maazkashif492@outlook.com>
2020-12-08 03:14:53 +0500

    Uploaded

---
+++ FakeMaxPlayers/FakeMaxPlayers/Config.cs
@@ -0,0 +1,19 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeMaxPlayers
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public int CustomMaxPlayers { get; set; }
+
+        public void LoadDefaults()
+        {
+            CustomMaxPlayers = 48;
+        }
+    }
+}
---
+++ FakeMaxPlayers/FakeMaxPlayers/Main.cs
@@ -0,0 +1,53 @@
+﻿using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeMaxPlayers
+{
+    public class Main : RocketPlugin<Config>
+    {
+        protected override void Load()
+        {
+            Logger.Log("FakeMaxPlayers has been loaded");
+
+            Level.onPostLevelLoaded += OnLevelLoaded;
+
+            if (Level.isLoaded)
+            {
+                SetMaxPlayers(Configuration.Instance.CustomMaxPlayers);
+            }
+        }
+
+        protected override void Unload()
+        {
+            Logger.Log("FakeMaxPlayers has been unloaded");
+
+            Level.onPostLevelLoaded -= OnLevelLoaded;
+        }
+
+        private void OnLevelLoaded(int level)
+        {
+            SetMaxPlayers(Configuration.Instance.CustomMaxPlayers);
+        }
+
+        public void SetMaxPlayers(int amount)
+        {
+            try
+            {
+                typeof(Provider)?.GetField("_maxPlayers", BindingFlags.NonPublic | BindingFlags.Static)
+                    ?.SetValue(null, amount);
+                Logger.Log($"Set Max Players to {amount}");
+            }
+            catch (Exception e)
+            {
+                Logger.LogException(e, "Issue changing max players");
+            }
+        }
+    }
+}
